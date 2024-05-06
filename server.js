c5ba3da7b5509eae926339c17944d57da9221055
Venkatesan N <akashvenkat47@gmail.com>
2024-05-06 21:11:40 +0530

    added database config and initialization

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/health", (req, res) => {
+    console.log("at the Health api endpoint");
+    res.json({
+        service: "Backend Job Listing Api server",
+        status: "active",
+        time: new Date(), 
+    });
+});
+
+app.use((error, req, res, next) => {
+    console.log(error.message);
+    res.status(500).json({
+        message: "Something went wrong!"
+    });
+});
+
 mongoose.connect(process.env.Mongodb_uri)
     .then(() => {
         console.log('Connected to database');
